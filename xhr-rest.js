cc84e9c336b30efd9066207fd6526845f3d20c75
Chris Stead <cmstead@gmail.com>
2015-11-07 12:45:07 -0800

    Completed XHR service.

--- xhr-rest.js
+++ xhr-rest.js
@@ -1,40 +1,86 @@
 var xhr = (function () {
+	'use strict';
 	
-	function buildParamPair (paramData, paramList, key) {
-		paramList.push(key + '=' + paramData[key].toString());
+	function buildParamPair (requestData, paramList, key) {
+		paramList.push(key + '=' + requestData[key].toString());
 		return paramList;
 	}
 	
-	function buildGetParams (paramData) {
-		return Object.keys(paramData).reduce(buildParamPair.bind(null, paramData)).join('&');
+	function buildGetParams (requestData) {
+		return Object.keys(requestData).reduce(buildParamPair.bind(null, requestData)).join('&');
 	}
 	
-	function buildRequestBody (paramData) {
-		return typeof paramData === 'object' ? JSON.stringify(paramData) : undefined;
+	function buildRequestBody (requestData) {
+		return typeof requestData === 'object' ? JSON.stringify(requestData) : undefined;
 	}
 	
 	function buildGetUrl (baseUrl, options) {
-		var urlParams = buildGetParams(options.requestData);
+		var urlParams = buildGetParams(options.data);
 		return baseUrl + (urlParams === '' ? '' : '?' + urlParams);
 	}
 	
-	function request (method, url, options) {
-		
+	function isNotNullObject (obj) {
+		return typeof obj === 'object' && obj !== null;
 	}
 	
-	function delete () {}
+	function sanitizeOptions (options) {
+		var defaultOptions = {
+				data: {},
+				headers: {},
+				stateActions: {
+					
+				}
+			},
+			requestOptions = isNotNullObject(options) ? options : defaultOptions,
+			derefData = requestOptions.data,
+			derefHeaders = requestOptions.headers;
+		
+		requestOptions.data = isNotNullObject(derefData) ? derefData : defaultOptions.data;
+		requestOptions.headers = isNotNullObject(derefHeaders) ? derefHeaders : defaultOptions.headers;
+	}
 	
-	function get () {}
+	function readyStateHandler(xhrObj, callback) {
+		var done = xhrObj.DONE === xhrObj.readyState,
+			status = done ? xhrObj.status : null,
+			response = done ? xhrObj.response : null,
+			error = done && response !== null ? null : { 
+				error: new Error ('Request was unsuccessful and returned status ' + status),
+				status: status
+			};
+
+		if(done) {
+			callback(error, response);
+		}
+	}
 	
-	function post () {}
+	function setXHRHeaders (xhrObj, headers) {
+		Object.keys(headers).forEach(function (key) {
+			xhrObj.setRequestHeader(key, headers[key]);
+		});
+	}
 	
-	function put () {}
+	function request (method, url, callback, options) {
+		var requestOptions = sanitizeOptions(options),
+			isDataRequest = ['GET', 'DELETE'].includes(method.toUpperCase()),
+			requestUrl = !isDataRequest ? buildGetUrl(url, options) : url,
+			xhrObj = new XMLHttpRequest();
+		
+		xhrObj.open(method.toUpperCase(), requestUrl);
+		setXHRHeaders(xhrObj, requestOptions.headers);
+		xhrObj.onreadystatechange = readyStateHandler.bind(null, xhrObj, callback);
+		
+		if (isDataRequest) {
+			xhrObj.send(buildRequestBody(options.data));
+		} else {
+			xhrObj.send();
+		}
+	}
 	
 	return {
-		delete: delete,
-		get: get,
-		post: post,
-		put: put,
+		delete: request.bind(null, 'DELETE'),
+		get: request.bind(null, 'GET'),
+		post: request.bind(null, 'POST'),
+		put: request.bind(null, 'PUT'),
 		request: request
 	};
 })();
