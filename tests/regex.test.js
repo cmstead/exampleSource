7df85232f3e2f1c9e6512a9d5976a8e8062e129c
Chris Stead <cmstead@gmail.com>
2016-03-13 20:58:53 -0700

    Added regex examples and tests

--- tests/regex.test.js
+++ tests/regex.test.js
@@ -12,64 +12,64 @@ describe('Regex examples', function () {
         this.verify(regexExample.loremIpsum);
     });
 
-    // it('should pick a simple instance of lorem', function () {
-    //     var pattern = /lorem/;
-    //     var result = patternMatcher(pattern);
-    //     this.verify(utils.prettyJson(result));
-    // });
+    it('should pick a simple instance of lorem', function () {
+        var pattern = /lorem/;
+        var result = patternMatcher(pattern);
+        this.verify(utils.prettyJson(result));
+    });
 
-    // it('should pick a case insensitive instance of lorem', function () {
-    //     var pattern = /lorem/i;
-    //     var result = patternMatcher(pattern);
-    //     this.verify(utils.prettyJson(result));
-    // });
+    it('should pick a case insensitive instance of lorem', function () {
+        var pattern = /lorem/i;
+        var result = patternMatcher(pattern);
+        this.verify(utils.prettyJson(result));
+    });
 
-    // it('should pick all instances of lorem', function () {
-    //     var pattern = /lorem/ig;
-    //     var result = patternMatcher(pattern);
-    //     this.verify(utils.prettyJson(result));
-    // });
+    it('should pick all instances of lorem', function () {
+        var pattern = /lorem/ig;
+        var result = patternMatcher(pattern);
+        this.verify(utils.prettyJson(result));
+    });
 
-    // it('should pick lorem or lorem ipsum', function () {
-    //     var pattern = /lorem( ipsum)?/ig;
-    //     var result = patternMatcher(pattern);
-    //     this.verify(utils.prettyJson(result));
-    // });
+    it('should pick lorem or lorem ipsum', function () {
+        var pattern = /lorem( ipsum)?/ig;
+        var result = patternMatcher(pattern);
+        this.verify(utils.prettyJson(result));
+    });
 
-    // it('should pick anything starting with l and ending with m', function () {
-    //     var pattern = /l.*m/ig;
-    //     var result = patternMatcher(pattern);
-    //     this.verify(utils.prettyJson(result));
-    // });
+    it('should pick anything starting with l and ending with m', function () {
+        var pattern = /l.*m/ig;
+        var result = patternMatcher(pattern);
+        this.verify(utils.prettyJson(result));
+    });
 
-    // it('should pick anything starting with l and ending with m which contains no spaces', function () {
-    //     var pattern = /l[^\s]*m/ig;
-    //     var result = patternMatcher(pattern);
-    //     this.verify(utils.prettyJson(result));
-    // });
+    it('should pick anything starting with l and ending with m which contains no spaces', function () {
+        var pattern = /l[^\s]*m/ig;
+        var result = patternMatcher(pattern);
+        this.verify(utils.prettyJson(result));
+    });
 
-    // it('should pick only the first lorem', function () {
-    //     var pattern = /^l[^\s]*m/ig;
-    //     var result = patternMatcher(pattern);
-    //     this.verify(utils.prettyJson(result));
-    // });
+    it('should pick only the first lorem', function () {
+        var pattern = /^l[^\s]*m/ig;
+        var result = patternMatcher(pattern);
+        this.verify(utils.prettyJson(result));
+    });
 
-    // it('should pick nothing', function () {
-    //     var pattern = /^l[^\s]*m$/ig;
-    //     var result = patternMatcher(pattern);
-    //     this.verify(utils.prettyJson(result));
-    // });
+    it('should pick nothing', function () {
+        var pattern = /^l[^\s]*m$/ig; // 'lorem' or 'lum'
+        var result = patternMatcher(pattern);
+        this.verify(utils.prettyJson(result));
+    });
 
-    // it('should pick each non-space token', function () {
-    //     var pattern = /[^\s]+/ig;
-    //     var result = patternMatcher(pattern);
-    //     this.verify(utils.prettyJson(result));
-    // });
+    it('should pick each non-space token', function () {
+        var pattern = /[^\s]+/ig;
+        var result = patternMatcher(pattern);
+        this.verify(utils.prettyJson(result));
+    });
 
-    // it('should pick every token containing an l and maybe an m', function () {
-    //     var pattern = /[^\s]*l[^\s]*m?/ig;
-    //     var result = patternMatcher(pattern);
-    //     this.verify(utils.prettyJson(result));
-    // });
+    it('should pick every token containing an l and maybe an m', function () {
+        var pattern = /[^\s]*l[^\s]*m?/ig;
+        var result = patternMatcher(pattern);
+        this.verify(utils.prettyJson(result));
+    });
 
 });
